2ea1aef7a89db498af40f19e383f3c3e140cc270
Brian <brian.zheng@titansoft.com.sg>
2018-08-03 19:22:46 +0800

    refactor by extract method

--- TennisGame0803/UnitTest1.cs
+++ TennisGame0803/UnitTest1.cs
@@ -5,12 +5,17 @@ namespace TennisGame0803
     [TestClass]
     public class TennisGameTests
     {
+        private TennisGame tennisGame = new TennisGame();
+
         [TestMethod]
         public void Love_All()
         {
-            TennisGame tennisGame = new TennisGame();
-            var score = tennisGame.GetScore();
-            Assert.AreEqual("Love_All", score);
+            ScoreShouldBe("Love_All");
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, tennisGame.GetScore());
         }
     }
 }
